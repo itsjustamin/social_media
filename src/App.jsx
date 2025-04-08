b0d05c4fcada6a147b6a9ff03bc58cded51f0fac
Amin <itsjustaminjon@gmail.com>
2025-04-08 08:25:01 +0500

    refactor: move nav components to separate folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./App.css";
-import Header from "./components/Header";
-import Navbar from "./components/Nav";
-import Profile from "./components/Profile";
+import Profile from "./components/Profile/Profile";
+import Navbar from "./components/Nav/Nav";
+import Header from "./components/Header/Header";
 
 const App = () => {
   return (
